4995142db6c63cab786bf91ec53bbbc7b8d97fb5
Dakota Martinez <dakotaleemusic@gmail.com>
2018-07-06 23:32:11 -0700

    remove test helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,11 +19,7 @@ class ApplicationController < Sinatra::Base
     def current_user
       session[:user_id] && User.find_by(id: session[:user_id])
     end
-
-    def test
-      "hello"
-    end
-
+    
     def logged_in?
       !!current_user
     end
